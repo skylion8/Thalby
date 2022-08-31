ce1d7b06bdb4533e158830aed8b35bd8bd59e0a7
skylion8 <ksobko@ukr.net>
2022-08-31 13:49:20 +0300

    dialoge window done

--- js/script.js
+++ js/script.js
@@ -19,3 +19,20 @@ $(document).ready(function(){
         }
     });
 });
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
